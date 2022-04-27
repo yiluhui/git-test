0a339b2c5e9d198afaa429939863ee327c530a01
h_hui <h_hui@qq.com>
2022-04-27 13:14:56 +0800

    first commit4

---
+++ git-test/src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,10 @@
+package com.atguigu;
+
+/**
+ * @Date 2022/4/27
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello gitl");
+    }
+}
---
+++ git-test0/src/main/java/com/atguigu.java
@@ -0,0 +1,12 @@
+package com;
+
+/**
+ * @Date 2022/4/27
+ */
+public class atguigu {
+    public static void main(String[] args) {
+        System.out.println("hello girl");
+        System.out.println("hello girl1");
+        System.out.println("hello girl1");
+    }
+}
